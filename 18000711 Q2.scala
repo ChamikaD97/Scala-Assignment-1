d8c6cf05b837cad795094ce5d7ee52ea88c2ce07
Chamika Deshan Jayasingha <56592121+ChamikaD97@users.noreply.github.com>
2020-05-24 09:39:09 +0530

    Create 18000711 Q2.scala

---
+++ 18000711 Q2.scala
@@ -0,0 +1,7 @@
+
+/*2.The volume of a sphere with radius r is 4/3 Pi r3. What is the volume of a sphere with radius 5?*/
+
+object Q2 extends App {
+  def vol(r : Double):Double=(4*22*r*r*r)/(3*7);
+  print("Volume of a sphere in radius 5 : "+vol(5));
+}
